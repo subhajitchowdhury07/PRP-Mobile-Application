76b49bab6d3ace1205116d22e682dc715c5afc68
subhajit chowdury <codinghacker2001@gmail.com>
2024-05-06 19:11:44 +0530

    add new page 'after login page'

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import '../ui/main_view.dart';
+import 'package:flutter/services.dart';
+import '../ui/screens/after_login_screen.dart'; // Import the AfterLoginScreen widget
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    // Set orientation to portrait
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.light(),
-      home: const MainView(),
+      home: AfterLoginScreen(), // Set the AfterLoginScreen as the home screen
     );
   }
 }
---
+++ lib/ui/screens/after_login_screen.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swipe_button/flutter_swipe_button.dart';
+
+class AfterLoginScreen extends StatelessWidget {
+  const AfterLoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        const SizedBox(height: 60.0),
+                        const Text(
+                          "Provisional Receipt",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          "Fill up the form and proceed to Payment",
+                          style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        _buildDateField(context, "Date", Icons.calendar_today),
+                        _buildFormField("Office/Lane", Icons.location_on),
+                        _buildFormField("Cash amount (USD)", Icons.attach_money),
+                        _buildFormField("Check amount (USD)", Icons.attach_money),
+                        _buildFormField("Total amount (USD)", Icons.attach_money),
+                        _buildFormField("Customer name", Icons.person),
+                        _buildFormField("Customer code", Icons.person),
+                        _buildFormField("Legal Entity / First and Last Names", Icons.person),
+                        _buildDepositDetailsField("Deposit details"),
+                        _buildSignatureField("Signature (Delivered)"),
+                        _buildSignatureField("Signature (Received)"),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    // SwipeButton replacement
+                    SwipeButton.expand(
+                      thumb: Icon(
+                        Icons.double_arrow_rounded,
+                        color: Colors.white,
+                      ),
+                      child: Text(
+                        "Swipe to Proceed Payment...",
+                        style: TextStyle(
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                      activeThumbColor: Colors.green,
+                      activeTrackColor: Colors.grey.shade300,
+                      onSwipe: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("Swiped"),
+                            backgroundColor: Colors.green,
+                          ),
+                        );
+                        // Navigate to payment page
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.article),
+              label: 'Form',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              label: 'History',
+            ),
+          ],
+          selectedItemColor: Colors.purple,
+          onTap: (int index) {
+            // Handle navigation to form or payment history
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDateField(BuildContext context, String hintText, IconData prefixIcon) {
+    final TextEditingController _dateController = TextEditingController();
+
+    return Column(
+      children: <Widget>[
+        const SizedBox(height: 20),
+        TextField(
+          onTap: () async {
+            DateTime? pickedDate = await showDatePicker(
+              context: context,
+              initialDate: DateTime.now(),
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2101),
+            );
+            if (pickedDate != null) {
+              String formattedDate = "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+              _dateController.text = formattedDate;
+            }
+          },
+          readOnly: true,
+          controller: _dateController,
+          decoration: InputDecoration(
+            hintText: hintText,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Colors.purple.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(prefixIcon),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFormField(String hintText, IconData prefixIcon) {
+    return Column(
+      children: <Widget>[
+        const SizedBox(height: 20),
+        TextField(
+          decoration: InputDecoration(
+            hintText: hintText,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Colors.purple.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(prefixIcon),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDepositDetailsField(String headingText) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const SizedBox(height: 20),
+        Text(
+          headingText,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        _buildFormField("Bank", Icons.account_balance),
+        _buildFormField("Drawer Name", Icons.person),
+        _buildFormField("Ch/. No.", Icons.label),
+        _buildFormField("Amount USD", Icons.attach_money),
+      ],
+    );
+  }
+
+  Widget _buildSignatureField(String headingText) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const SizedBox(height: 20),
+        Text(
+          headingText,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        Container(
+          height: 150, // Adjust height as needed
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          // Placeholder for digital signature field
+        ),
+      ],
+    );
+  }
+}
